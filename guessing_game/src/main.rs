e167766928c711e81a227c933be1c383927539c0
baranov-V-V <bar.victor.2002@gmail.com>
2025-05-28 13:45:33 +0300

    feat: added final version of guessing-game program

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,33 @@
+use std::cmp::Ordering;
 use std::io;
+
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please insert you guess!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please insert you guess!");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+        println!("You guessed: {}", guess);
+    }
 }
